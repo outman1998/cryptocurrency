a3c93b44a47bf4a0aa221b126f38b81ae71bb2a1
outman1998 <outmanmounir98@gmail.com>
2023-08-31 22:54:33 +0200

    some work on single one page

--- src/pages/Coinpage.js
+++ src/pages/Coinpage.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import {useCurrency} from '../context/context';
+import axios from 'axios';
+import {SingleCoin} from '../config/api';
+import { makeStyles } from '@material-ui/core';
+import CoinInfo from '../components/CoinInfo';
 
 export default function Coinpage() {
+
+  const {id} = useParams();
+  const [coin, setCoin] = useState();
+  const {currency} = useCurrency();
+
+  const fetchCoin = async () => {
+  const {data} = await axios.get(SingleCoin(id));
+  setCoin(data);
+  }
+
+  console.log(coin);
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
+  const useStyles = makeStyles(() => {
+    
+  });
+
+  const classes = useStyles();
+
   return (
-    <h1>Coinpage</h1>
+    <div className={classes.container}>
+      <div className={classes.sidebar}>
+        {/* {sidebar} */}
+      </div>
+
+      {/* {chart} */}
+      <CoinInfo coin={coin} />
+    </div>
   )
 }
